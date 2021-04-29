6c3620d39dac2a3a991c50f1736e5ffe0418d1d6
dia <tagadia86@gmail.com>
2021-04-30 00:16:21 +0200

    personnalisation du vernis

--- FRONTEND/item.js
+++ FRONTEND/item.js
@@ -1,8 +1,8 @@
 
   let urlSearch = new URLSearchParams(window.location.search);
-  console.log(urlSearch);
+  //console.log(urlSearch);
   let selectedCam = urlSearch.get('id');
-  console.log(selectedCam);
+  //console.log(selectedCam);
 
   
 let mainBlock = document.getElementById("main_block");
@@ -24,86 +24,85 @@ const get = (url)=>
 };
 
 get("http://localhost:3000/api/furniture").then((response)=>{
-  console.log(response);
+  //console.log(response);
   displayChoosen(response);
 })
 
 const displayChoosen = (furnitures)=>
 {
-  let cpt = 0;
   furnitures.forEach(furniture => 
   {
-    cpt+=1;
     if (furniture._id == selectedCam )
     {
-    console.log(cpt);
+      // card main div for each item
+      let card_wrapper = document.createElement("div");
+      card_wrapper.classList.add("container","col-4");
+      main_block.appendChild(card_wrapper);
 
-    // card main div for each item
-    let card_wrapper = document.createElement("div");
-    card_wrapper.classList.add("container","col-4");
-    main_block.appendChild(card_wrapper);
+      // card second div for each item
+      let card_wrapper_child = document.createElement("div");
+      card_wrapper_child.classList.add("card", "mb-4", "mb-lg-0", "border-light", "shadow-sm");
+      card_wrapper.appendChild(card_wrapper_child);
 
-    // card second div for each item
-    let card_wrapper_child = document.createElement("div");
-    card_wrapper_child.classList.add("card", "mb-4", "mb-lg-0", "border-light", "shadow-sm");
-    card_wrapper.appendChild(card_wrapper_child);
+      //appending the card image
+      let imageCamera = document.createElement("img");
+      imageCamera.classList.add("card-img-top");
+      card_wrapper_child.appendChild(imageCamera);
+      imageCamera.src = furniture.imageUrl;
+      
+      // creating description wrapper
+      let description_wrapper = document.createElement("div"); 
+      description_wrapper.classList.add("card-body");
+      card_wrapper_child.appendChild(description_wrapper);
 
-    //appending the card image
-    let imageCamera = document.createElement("img");
-    imageCamera.classList.add("card-img-top");
-    card_wrapper_child.appendChild(imageCamera);
-    imageCamera.src = furniture.imageUrl;
-    
-     // creating description wrapper
-     let description_wrapper = document.createElement("div"); 
-     description_wrapper.classList.add("card-body");
-     card_wrapper_child.appendChild(description_wrapper);
+      //appending the card title
+      let itemName = document.createElement("h5");
+      itemName.classList.add("card-title");
+      card_wrapper_child.appendChild(itemName);
+      itemName.innerHTML += furniture.name;
 
-    //appending the card title
-     let itemName = document.createElement("h5");
-     itemName.classList.add("card-title");
-     card_wrapper_child.appendChild(itemName);
-     itemName.innerHTML += furniture.name;
+      //appending the card title
+      let itemPrice = document.createElement("p");
+      itemPrice.classList.add("card-text");
+      card_wrapper_child.appendChild(itemPrice);
+      itemPrice.innerHTML += furniture.price;
 
-     //appending the card title
-     let itemPrice = document.createElement("p");
-     itemPrice.classList.add("card-text");
-     card_wrapper_child.appendChild(itemPrice);
-     itemPrice.innerHTML += furniture.price;
+      //appending the card id
+      let itemId = document.createElement("p");
+      itemId.classList.add("card-text");
+      card_wrapper_child.appendChild(itemId);
+      itemId.innerHTML += furniture._id;
 
-    //appending the card id
-     let itemId = document.createElement("p");
-     itemId.classList.add("card-text");
-     card_wrapper_child.appendChild(itemId);
-     itemId.innerHTML += furniture._id;
+      //appending the card title
+      let itemDescription = document.createElement("p");
+      itemDescription.classList.add("card-text");
+      card_wrapper_child.appendChild(itemDescription);
+      itemDescription.innerHTML += furniture.description;
 
-    //appending the card title
-     let itemDescription = document.createElement("p");
-     itemDescription.classList.add("card-text");
-     card_wrapper_child.appendChild(itemDescription);
-     itemDescription.innerHTML += furniture.description;
+      //choosing one option
+      let formContainer = document.createElement("div");
+      card_wrapper_child.appendChild(formContainer);
+      let formulaire = document.createElement("form");
+      formContainer.appendChild(formulaire);
+      formulaire.innerHTML = "choisissez votre vernis";
+      let selectElement = document.createElement("select");
+      selectElement.id = "mySelect";
+      formulaire.appendChild(selectElement);
+      furniture.varnish.forEach(eachVarnish => {
+          console.log(eachVarnish);
+          let optionElement = document.createElement("option")
+          selectElement.appendChild(optionElement);
+          optionElement.value += eachVarnish;
+          optionElement.innerHTML += eachVarnish;
+        });
 
-    //creating the button
-    let itemButton = document.createElement("a");
-    itemButton.classList.add("btn","btn-primary","stretched-link");
-    card_wrapper_child.appendChild(itemButton);
-    itemButton.innerHTML += "Ajouter au panier";
-    itemButton.href = "item.html?id="+furniture._id;
-    itemButton.role = "button";
-
-    //choosing one option
-    let formContainer = document.createElement("div");
-    
-    let formulaire = document.createElement("form");
-    formContainer.appendChild(formulaire);
-    formulaire.innerHTML = "choisissez votre option";
-    let selectElement = document.createElement("select");
-    selectElement.id = "mySelect";
-    formulaire.appendChild(selectElement);
-    let optionElement = document.createElement("option")
-    optionElement.value ="apple";
-    selectElement.appendChild(optionElement);
-
+      //creating the button
+      let itemButton = document.createElement("a");
+      itemButton.classList.add("btn","btn-primary");
+      card_wrapper_child.appendChild(itemButton);
+      itemButton.innerHTML += "Ajouter au panier";
+      itemButton.href = "item.html?id="+furniture._id;
+      itemButton.role = "button";
     }
   });
 
