24c9389637af85f6d5a54c6a42ef85f8ef1325a0
dia <tagadia86@gmail.com>
2021-05-07 04:40:58 +0200

    affichage de la quantité à mettre à jour

--- FRONTEND/cart.js
+++ FRONTEND/cart.js
@@ -43,19 +43,48 @@ const displayItem = (furnitures)=>
 
     let itemName = document.createElement("p");
     cardBody.appendChild(itemName);
-    itemName.innerHTML += furniture.name;
+    itemName.innerHTML += "nom = " + furniture.name;
 
     let itemPrice = document.createElement("p");
     cardBody.appendChild(itemPrice);
-    itemPrice.innerHTML += furniture.price;
+    itemPrice.innerHTML += "prix = " + furniture.price;
 
     let itemID = document.createElement("p");
     cardBody.appendChild(itemID);
-    itemID.innerHTML += furniture._id;
+    itemID.innerHTML += "id=" + furniture._id;
 
     let itemDescription = document.createElement("p");
     cardBody.appendChild(itemDescription);
-    itemDescription.innerHTML += furniture.description + "<br>";
+    itemDescription.innerHTML += "description : " + furniture.description + "<br>";
+
+        //appending the minus button
+        let minusButton = document.createElement("a");
+        minusButton.classList.add("btn","btn-primary");
+        cardBody.appendChild(minusButton);
+        minusButton.innerHTML += "diminuer";
+        minusButton.role = "button";
+    
+        minusButton.addEventListener('click', function() 
+        {   
+          
+          let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
+          furniture.count -=1;
+          if (furniture.count == 0)
+          {
+            //let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
+            removeChoosen (shoppingCart,furniture._id);
+            localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+            location.reload(); 
+          }
+    
+        });
+    
+        /*END: minus button */
+
+    //item quantity
+    let itemQuantity = document.createElement("p");
+    cardBody.appendChild(itemQuantity);
+    itemQuantity.innerHTML += "quantité = " + furniture.count + "<br>";
 
     //appending the remove button
     let removeButton = document.createElement("a");
@@ -63,6 +92,7 @@ const displayItem = (furnitures)=>
     cardBody.appendChild(removeButton);
     removeButton.innerHTML += "supprimer cet article";
     removeButton.role = "button";
+
     //calling the function to remove an item on click
     removeButton.addEventListener('click', function() 
     {   
@@ -72,13 +102,15 @@ const displayItem = (furnitures)=>
       location.reload();
     });
     /*END: remove button section */
+
+
+
   });
   /*END: loop to display items in the cart */
 }
 
 //calling the function to display items in the cart
 displayItem(shoppingCart);
-
 
 //appending the clear button
 let clearButton = document.createElement("a");
@@ -96,51 +128,35 @@ clearButton.addEventListener('click', function()
 });
 /*END: clear button section */
 
-
 //function to remove item on click
-const removeChoosen = (removingArray,itemToRemove)=>
-{
- //console.log("removed function called");
+const removeChoosen = (removingArray,itemToRemove)=>{
   let cptRemove = 0;
-  removingArray.forEach(index => 
-  {
-    //console.log("I'm in the loop to find the item to remove");
-    if (index._id == itemToRemove )
-    {
-      //console.log("ID de l'index");
+  removingArray.forEach(index => {
+    if (index._id == itemToRemove ){
       console.log(index._id);
-      //let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
-      if (removingArray === null) 
-      {
+      if (removingArray === null) {
           removingArray = [];
       }
       removingArray.splice(cptRemove, 1); 
-      //removingArray.pop(); 
-      //localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
-      //location.reload();
     }
     cptRemove++;
-    console.log(cptRemove);
-    setTimeout(() => {  console.log("PAUSE"); }, 200000);
   });
 }
 /*end of the function to remove item on click*/
 
 //function to clear the cart
 const clearCart = (cartToClear)=>{
-  while (cartToClear.length > 0)
-  {
-    //let cartToClear = JSON.parse(localStorage.getItem("shoppingCart"));
-    if (cartToClear === null) 
-    {
+  while (cartToClear.length > 0){
+    if (cartToClear === null) {
         cartToClear = [];
     }
     cartToClear.pop();
-    //localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
     location.reload();
   }
 }
 /*END: function to clear the cart */
+
+
 
 
 
